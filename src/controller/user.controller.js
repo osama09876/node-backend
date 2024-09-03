b0f5e12dcaf8add334c93feb1d58ba3781881d35
Muhammad Osama Farooqui <osamafarooqui0987@gmail.com>
2024-09-03 20:08:45 +0500

    write register user functionality and resolve bugs

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,10 +1,66 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadToCloudinary } from "../utils/cloudnary.js";
+import ApiResponse from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // Your registration logic here
-  res.status(200).json({
-    message: "OK",
+  // get details from user
+  //check user already registered or not registered
+  //validation used
+  //avatar is required
+  //get avatar from user
+  //upload to cloudinary
+  //save user to database
+  //remove user pasword and refresh token from response
+  //return response
+
+  const { userName, email, fullName, password } = req.body;
+  // res.json({ message: "success" });
+  // console.log(email);
+  if (
+    [userName, email, fullName, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ email }, { userName }],
   });
+  if (existedUser) {
+    throw new ApiError(409, "User already exists");
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  const avatar = await uploadToCloudinary(avatarLocalPath);
+  const coverImage = await uploadToCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  const user = await User.create({
+    userName: userName.toLowerCase(),
+    email,
+    fullName,
+    avatar: avatar?.url || "",
+    coverImage: coverImage?.url || "",
+    password,
+  });
+  const userRegisterd = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!userRegisterd) {
+    throw new ApiError(500, "User registration failed");
+  }
+
+  res
+    .status(201)
+    .json(ApiResponse(200, userRegisterd, "User registered successfully"));
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { registerUser } from "../controller/user.controller.js";
+import { upload } from "../middleware/multer.middleware.js";
+import { ApiError } from "../utils/apiError.js";
 
 const userRouter = Router();
 
-userRouter.route("/register").post(registerUser);
+userRouter.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
 export default userRouter;
