f5a88da3bb08c0f3bfdbbc118146f7184ef63633
Muhammad Osama Farooqui <osamafarooqui0987@gmail.com>
2024-09-08 11:38:20 +0500

    setup all routes and controllers

--- src/app.js
+++ src/app.js
@@ -19,5 +19,22 @@ app.use(cookiePaser());
 //routes
 
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import tweetRouter from "./routes/tweet.route.js";
+import likeRouter from "./routes/like.route.js";
+import commentRouter from "./routes/comment.route.js";
+import dashboardRouter from "./routes/dashboard.route.js";
+import healthcheckRouter from "./routes/healthcheck.route.js";
+import palylistRouter from "./routes/playlist.route.js";
+import { plugin } from "mongoose";
 app.use(`${API_NAME}/users`, userRouter);
+app.use(`${API_NAME}/videos`, videoRouter);
+app.use(`${API_NAME}/subscription`, subscriptionRouter);
+app.use(`${API_NAME}/tweets`, tweetRouter);
+app.use(`${API_NAME}/likes`, likeRouter);
+app.use(`${API_NAME}/comments`, commentRouter);
+app.use(`${API_NAME}/dashboard`, dashboardRouter);
+app.use(`${API_NAME}/healthcheck`, healthcheckRouter);
+app.use(`${API_NAME}/playlists`, palylistRouter);
 export { app };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -15,7 +15,7 @@ import {
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
-const userRouter = Router();
+const router = Router();
 
 userRouter.route("/register").post(
   upload.fields([
@@ -31,21 +31,19 @@ userRouter.route("/register").post(
   registerUser
 );
 
-userRouter.route("/login").post(loginUser);
-userRouter.route("/logout").post(verifyJWT, logoutUser);
-userRouter.route("/refresh-token").post(refreshAccessToken);
-userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword);
-userRouter.route("/get-current-user").get(verifyJWT, getCurrentUser);
-userRouter.route("/update-account").patch(verifyJWT, updateUserInfo);
-userRouter
-  .route("/avatar")
-  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
-userRouter
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/get-current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateUserInfo);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
 // userRouter.route("/c/:username").get(verifyJWT, getUserChannelDetails);
-userRouter.route("/c/:username").get(verifyJWT, getUserChannelDetails);
-userRouter.route("/history").get(verifyJWT, getUserWatchHistory);
+router.route("/c/:username").get(verifyJWT, getUserChannelDetails);
+router.route("/history").get(verifyJWT, getUserWatchHistory);
 
-export default userRouter;
+export default router;
